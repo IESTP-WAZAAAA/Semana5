00b375e3d9f4a42f56424d81f0ddd6dc08bbf29c
Jhon Leonardo Lozada Huamán <lozadahuamanj@iestptrujillo.net>
2024-09-19 11:01:22 -0500

    Mi primer comit

---
+++ WEAPPI/Controllers/alumno.cs
@@ -0,0 +1,50 @@
+﻿namespace WEAPPI.Controllers
+{
+    public class Alumno
+    {
+        //crear las propiedades
+        string nombre;
+        string apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        //constructor sin parametros
+        public Alumno()
+        {
+            nombre = string.Empty;
+            apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+
+        //constructor con parametro
+        public Alumno(string pnombre, string papeliido, string pdni)
+        {
+            nombre = pnombre;
+            apellido = papeliido;
+            dni = pdni;
+        }
+
+        public Alumno(string pnombre, string papeliido, string pdni, DateTime pfechanacimiento)
+        {
+            nombre = pnombre;
+            apellido = papeliido;
+            dni = pdni;
+            fechanacimiento = pfechanacimiento;
+        }
+
+        public string DevolverNombreCompleto()
+        {
+            return nombre + " " + apellido;
+        }
+        
+        public string DevolverDni()
+        {
+            return dni;
+        }
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+    }
+}
